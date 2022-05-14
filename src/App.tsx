49ebdf9aa480216a5d56dec16039c8e6a573b6ee
Joel <joelluis73@gmail.com>
2022-05-14 11:34:33 -0300

    create Widget component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
+import { Widget } from './components/Widget'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const bla = 'klfjdkasf'
-
-  return <h1>Hello World</h1>
+  return <Widget />
 }
 
 export default App
