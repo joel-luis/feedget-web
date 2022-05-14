231d888c2407b338bf69c768c4928b06c3bd88fb
Joel <joelluis73@gmail.com>
2022-05-14 09:50:53 -0300

    install prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
 
-
 function App() {
   const [count, setCount] = useState(0)
+  const bla = 'klfjdkasf'
 
-  return (
-  <h1>Hello World</h1>
-  )
+  return <h1>Hello World</h1>
 }
 
 export default App
